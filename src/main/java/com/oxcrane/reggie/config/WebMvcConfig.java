4d5968dd784a33eedb7088d9698f5fe995cfd364
niuhe <389307471@qq.com>
2023-03-12 20:08:12 +0800

    黑马瑞吉外卖第二天—员工的crud

--- src/main/java/com/oxcrane/reggie/config/WebMvcConfig.java
+++ src/main/java/com/oxcrane/reggie/config/WebMvcConfig.java
@@ -1,9 +1,15 @@
 package com.oxcrane.reggie.config;
 
+import com.oxcrane.reggie.common.JacksonObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+import java.util.List;
+
 @Slf4j
 @Configuration
 public class WebMvcConfig extends WebMvcConfigurationSupport {
@@ -16,5 +22,20 @@ public class WebMvcConfig extends WebMvcConfigurationSupport {
         log.info("开始静态资源映射...");
         registry.addResourceHandler("/backend/**").addResourceLocations("classpath:/backend/");
         registry.addResourceHandler("/front/**").addResourceLocations("classpath:/front/");
+    }
+
+    /**
+     * 拓展mvc的消息转换器
+     * @param converters
+     */
+    @Override
+    protected void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
+        log.info("拓展消息转换器");
+//        创建消息转换器
+        MappingJackson2HttpMessageConverter messageConverter = new MappingJackson2HttpMessageConverter();
+//        设置对象转换器，底层将JAVA对象转换成JSON
+        messageConverter.setObjectMapper(new JacksonObjectMapper());
+//        将上面的消息转换器添加到MVC框架的转换器集合中
+        converters.add(0,messageConverter);
     }
 }
--- src/main/java/com/oxcrane/reggie/controller/EmployeeController.java
+++ src/main/java/com/oxcrane/reggie/controller/EmployeeController.java
@@ -1,19 +1,20 @@
 package com.oxcrane.reggie.controller;
 
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.oxcrane.reggie.common.R;
 import com.oxcrane.reggie.entity.Employee;
 import com.oxcrane.reggie.service.EmployeeService;
 import com.oxcrane.reggie.service.impl.EmployeeServiceImpl;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang.StringUtils;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.util.DigestUtils;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
+import java.time.LocalDateTime;
 
 @Slf4j
 @RestController
@@ -67,6 +68,90 @@ public class EmployeeController {
         request.getSession().removeAttribute("employee");
         return R.success("退出成功");
     }
+
+    @PostMapping
+    public R<String> save(HttpServletRequest request,@RequestBody Employee employee) {
+        log.info("新增员工，员工信息：{}" ,employee.toString());
+
+//        设置初始密码为123456，但需要md5加密
+        employee.setPassword(DigestUtils.md5DigestAsHex("123456".getBytes()));
+
+        employee.setCreateTime(LocalDateTime.now());
+        employee.setUpdateTime(LocalDateTime.now());
+
+        Long empId= (Long) request.getSession().getAttribute("employee");
+
+        employee.setCreateUser(empId);
+        employee.setUpdateUser(empId);
+
+        employeeService.save(employee);
+
+        return R.success("新增员工成功");
+    }
+
+    /**
+     * 员工信息分页查询
+     * @param page
+     * @param pageSize
+     * @param name
+     * @return
+     */
+    @GetMapping("/page")
+    public R<Page> page(int page, int pageSize, String name) {
+        log.info("page = {},pageSize = {}, name = {}",page,pageSize,name);
+
+//        构造分页构造器
+        Page pageInfo = new Page(page, pageSize);
+//        构造条件构造器
+        LambdaQueryWrapper<Employee> queryWrapper = new LambdaQueryWrapper<>();
+//        添加过滤条件
+        queryWrapper.like(StringUtils.isNotEmpty(name),Employee::getName,name);
+//        添加排序条件
+        queryWrapper.orderByDesc(Employee::getUpdateTime);
+//        执行查询
+        employeeService.page(pageInfo,queryWrapper);
+
+        return R.success(pageInfo);
+    }
+
+    /**
+     * 员工登录
+     * @param employee
+     * @return
+     */
+    @PutMapping
+    public R<String> update(HttpServletRequest request, @RequestBody Employee employee){
+        log.info(employee.toString());
+
+//        获取当修改用户的id
+        Long empId = (Long) request.getSession().getAttribute("employee");
+
+//        设置修改用户的id
+        employee.setUpdateUser(empId);
+
+//        设置更新的时间
+        employee.setUpdateTime(LocalDateTime.now());
+
+        employeeService.updateById(employee);
+
+        return R.success("员工信息修改成功");
+    }
+
+    /**
+     * 根据id查询员工信息
+     * @param id
+     * @return
+     */
+    @GetMapping("/{id}")
+    public R<Employee> getById(@PathVariable Long id){
+        log.info("根据id查询员工信息");
+        Employee employee = employeeService.getById(id);
+        if (employee != null) {
+            return R.success(employee);
+        }
+        return R.error("没有查询到对应信息");
+    }
+
 
 
 }
